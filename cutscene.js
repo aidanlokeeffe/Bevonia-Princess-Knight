0d9fd3cc29eb1a39b697453610643cf6a1ecc3ee
rgoparaju <ravi.goparaju@utexas.edu>
2018-12-02 15:26:04 -0600

    testing text stuff

--- cutscene.js
+++ cutscene.js
@@ -3,14 +3,14 @@ demo.cutscene.prototype = {
     preload: function () {
         // Load background
         game.load.image("bgCut", "assets/tilesets_backgrounds/cutscene background 2.5.png", 1198, 601);
-        game.load.image('bubble','assets/sprites/speech bubble.png')
+        game.load.image('bubble','assets/sprites/speech bubble.png',100,69)
         
         
     },
     create: function () {
         game.add.sprite(0, 0, "bgCut");
-        bubble1 = game.add.sprite(100,100,'bubble')
-        
+        bubble1 = game.add.sprite(300,200,'bubble')
+//        bubble1.scale.setTo(2,2)
         
         
         
