2ede469556b58a0320d7b4a466bdb3a177c59cbb
rgoparaju <ravi.goparaju@utexas.edu>
2018-12-02 15:21:48 -0600

    fit cutscene background to the game window

--- cutscene.js
+++ cutscene.js
@@ -2,14 +2,14 @@ demo.cutscene = function () {};
 demo.cutscene.prototype = {
     preload: function () {
         // Load background
-        game.load.image("bgCut", "assets/tilesets_backgrounds/cutscene background.png", 1198, 601);
-        
+        game.load.image("bgCut", "assets/tilesets_backgrounds/cutscene background 2.5.png", 1198, 601);
+        game.load.image('bubble','assets/sprites/speech bubble.png')
         
         
     },
     create: function () {
         game.add.sprite(0, 0, "bgCut");
-        
+        bubble1 = game.add.sprite(100,100,'bubble')
         
         
         
