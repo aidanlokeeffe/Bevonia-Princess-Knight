e2bf8e242e3f2ee2ba22226cefc4f6bca2ed4206
rgoparaju <ravi.goparaju@utexas.edu>
2018-12-02 21:15:46 -0600

    dialogue

--- cutscene.js
+++ cutscene.js
@@ -1,5 +1,6 @@
-var text1
-
+var text0,text1,text2,text3,text4,text5,  promptText
+var bubble1, bubble2
+var counter, spaceCounter
 
 
 demo.cutscene = function () {};
@@ -56,21 +57,104 @@ demo.cutscene.prototype = {
         
         // Text bubble stuff
         bubble1 = game.add.sprite(300,200,'bubble')
+        bubble2 = game.add.sprite(775,170,'bubble')
+        bubble2.scale.x = -1
+        bubble2.visible = false
 
+            text0 = new Typewriter()
         text1 = new Typewriter()
-        text1.init(game, {
+            text2 = new Typewriter()
+        text3 = new Typewriter()
+            text4 = new Typewriter()
+        text5 = new Typewriter()
+        text0.init(game, {
             x: 310,
             y: 210,
             fontFamily: 'augusta',
             fontSize: 12,
-            maxWidth: 190,
-            text: 'this is a test to see how the typing works. this is filler to see where the margins are cut off, and where the text ends. if you want to read me you can, but you probably won\'t get anything out of it. have a nice day'
+            maxWidth: 180,
+            text: 'The Nobles grow restless, Father. There seem to be signs of strife and conflict growing in the Kingdom.'
 
         })
-//        text1.start()
+        
+        
+        text2.init(game, {
+            x: 310,
+            y: 210,
+            fontFamily: 'augusta',
+            fontSize: 12,
+            maxWidth: 180,
+            text: 'What do you propose we shall do? We must act soon or else the kingdom may be thrown into turmoil. We must unite the Kingdom!'
+
+        })
+        
+        text4.init(game, {
+            x: 310,
+            y: 210,
+            fontFamily: 'augusta',
+            fontSize: 12,
+            maxWidth: 180,
+            text: 'That sounds like a wonderful plan, Father! I shall depart as soon as I can!'
+
+        })
+        
+        
+        text1.init(game, {
+            x: 590,
+            y: 180,
+            fontFamily: 'augusta',
+            fontSize: 12,
+            maxWidth: 175,
+            text: 'You are correct, Bevonia. I too, have noticed these most troublesome changes. Each day in court, the Nobles become more belligerent. I fear they may even be inciting strife among the ordinary citizens.'
+
+        })
+        
+        text3.init(game, {
+            x: 590,
+            y: 180,
+            fontFamily: 'augusta',
+            fontSize: 12,
+            maxWidth: 175,
+            text: 'We must make sure that all areas and parts of the Kingdom become united under one banner. I propose that you travel with an envoy to the other parts of the Kingdom to appeal to the lords who reside there.'
+
+        })
+        
+        text5.init(game, {
+            x: 590,
+            y: 180,
+            fontFamily: 'augusta',
+            fontSize: 12,
+            maxWidth: 175,
+            text: 'Excellent! I am glad you agree with me, Bevonia. Hopefully, with this new strategy, we can-'
+
+        })
+        
+        promptText = game.add.text(100,100,'Press \'SPACE\' to continue',{fontsize: '25px', fill: '#ffffff'})
+        promptText.font = 'augusta'
+        textArray = [text0,text1,text2,text3,text4,text5]
+        text0.start()
+        counter = 1
+        spaceCounter = 0
     },
     update: function () {
-        if(game.input.keyboard.isDown(Phaser.Keyboard.ENTER)) text1.start()
+        
+        if(game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR) && counter < textArray.length && spaceCounter < game.time.now){
+//            promptText.visible = false
+            if(counter%2 == 0){
+                bubble1.visible = true
+                bubble2.visible = false
+            }
+            else if(counter%2 == 1){
+                bubble1.visible = false
+                bubble2.visible = true
+            }
+            
+            textArray[counter-1].destroy()
+            textArray[counter].start()
+            counter++
+            spaceCounter = game.time.now + 100
+        }
+        
 
 
 
